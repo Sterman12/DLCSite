428386338b67348a5e9d492046831f9e09bc8ac3
Sterman12 <Botcreature1@gmail.com>
2026-04-03 23:11:42 -0500

    table sorting first commit

--- script.js
+++ script.js
@@ -1,6 +1,40 @@
+function sortTable(n) {
+table = document.getElementById("dlc_request_table");
+let table;
+let rows, i, x, y, count = 0;
+let switching = true;
+let direction = 'descending';
+while (switching) {
+    switching = false;
+    let rows = table.rows;
+    for(i=0; i<rows.length-1; i++) {
+        var Switch=false;
 
-async => ({
+        x = rows[i].getElementsByTagName('TD')[n];
+        y = rows[i + 1].getElementsByTagName('TD')[n];
 
-
-})
-
+        if (direction == 'ascending') {
+            if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+                Switch = true;
+                break;
+            }
+        }
+        else if (direction == 'descending') {
+            if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+                Switch = true;
+                break;
+            }
+        }
+    }
+if (Switch) {
+    rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+    switching = true;
+    count++;
+} else {
+          if (count == 0 && direction == 'ascending') {
+            direction = 'descending';
+            switching = true;
+            }
+        }
+    }
+}
