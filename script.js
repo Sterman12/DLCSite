787f95ded46ae9a31ff39f42f3dda4bea417e20e
Sterman12 <Botcreature1@gmail.com>
2026-04-04 01:14:36 -0500

    fixed time column sorting?

--- script.js
+++ script.js
@@ -8,11 +8,52 @@ while (switching) {
     switching = false;
     rows = table.rows;
     for(i=1; i<rows.length-1; i++) {
-        Switch=false;
+        Switch = false;
 
         x = rows[i].getElementsByTagName('TD')[n];
         y = rows[i + 1].getElementsByTagName('TD')[n];
-        if ((y === undefined) || (x === undefined)) {break;}
+        if ((y === undefined) || (x === undefined)) {break;} // added break statement prevent reading of undefined
+        if (direction == 'ascending') {
+            if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+                Switch = true;
+                break;
+            }
+        }
+        else if (direction == 'descending') {
+            if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+                Switch = true;
+                break;
+            }
+        }
+    }
+if (Switch) {
+    rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+    switching = true;
+    count++;
+} else {
+          if (count == 0 && direction == 'ascending') {
+            direction = 'descending';
+            switching = true;
+            }
+        }
+    }
+}
+function sortTableTime(n) {
+let table = document.getElementById("dlc_request_table");
+let rows;
+let i, x, y, count = 0;
+let switching = true;
+let direction = 'descending';
+while (switching) {
+    switching = false;
+    rows = table.rows;
+    for(i=1; i<rows.length-1; i++) {
+        Switch = false;
+
+        x = Date.parse(rows[i].getElementsByTagName('TD')[n]);
+        y = Date.parse(rows[i + 1].getElementsByTagName('TD')[n]);
+
+        if ((y === undefined) || (x === undefined)) {break;} // added break statement prevent reading of undefined
         if (direction == 'ascending') {
             if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
                 Switch = true;
